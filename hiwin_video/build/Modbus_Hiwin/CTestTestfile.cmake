5c3bf5b14dd2909d6cf469ccd469ee5e483db33a
taiwenlin <409500559@gms.tku.edu.tw>
2023-05-12 01:11:13 +0800

    add hiwin_video

--- hiwin_video/build/Modbus_Hiwin/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lintaiwen/hiwin_ws/src/Modbus_Hiwin
-# Build directory: /home/lintaiwen/hiwin_ws/build/Modbus_Hiwin
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
